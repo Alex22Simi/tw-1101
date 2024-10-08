6be91ab74355c3c64d9386c43035b7d712ec6398
Alex22Simi <simionescualexandru22@gmail.com>
2024-10-08 16:03:04 +0300

    first commit

---
+++ sem2.js
@@ -0,0 +1,17 @@
+//js -> limbaj programare nivel inalt = 
+//abstractizare cod(se ocupa singur de memeorie(curatenie))
+//cand citeste o linie o si compileaza
+//multi-paradigm = clase, functii etc
+//dynamic typing = nu se declareaza tipul variabilei
+//first-class functions. = functiile sunt tratate ca niste variabile
+//alert("Salut")
+//const number  = 3;
+//number
+//git init = creare repo local
+//git add . = adaugare
+//git commit -m "first commit" = urcare comit
+//git config --global user.email "simionescualexandru22@gmail.com
+//>> git config --global user.name "Alex22Simi"
+
+
+
